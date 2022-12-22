382d8e92e0c0a9aaeb3e970cde69fd91508a752f
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2022-12-22 23:58:01 +0200

    d3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
 
+const submitBtn = document.querySelector('.feedback-form button');
+const input = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea')
+
+submitBtn.addEventListener('click', onSubmit);
+
+function onSubmit(e) {
+
+    e.preventDefault();
+    
+        console.log({ 'Email': input.value, message: message.value, });
+        localStorage.clear("feedback-form-state");
+        input.value = '';
+        message.value = '';
+};
+
+input.addEventListener('input', onTyping);
+message.addEventListener('input', onTyping);
+
+function onTyping(e) {
+    const formEl = {
+    Email: input.textContent,
+    Message: message.textContent,
+};
+    window.localStorage.setItem("feedback-form-state", JSON.stringify(formEl));
+};
