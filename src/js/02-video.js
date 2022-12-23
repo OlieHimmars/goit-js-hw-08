77e134dba42aa4603260bc2bf04b5cfe8c22babd
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2022-12-23 16:49:36 +0200

    fix4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,24 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
  player.on('play', function() {
      console.log('played the video!');
 });
 
-const lock = function (data) { 
-     _.throttle(() => {
-    window.localStorage.setItem("videoplayer-current-time", data.seconds);
-    console.log(data);
-  }, 1000)
-    
-}
 
-player.on('timeupdate', lock);
-
-
+player.on('timeupdate', throttle(data => { localStorage.setItem('videoplayer-current-time', data.seconds)}, 1000)
+);
 
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
-   // localStorage.clear("videoplayer-current-time");
-    
+   
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
             // the time was less than 0 or greater than the video’s duration
             break;
-
         default:
             // some other error occurred
             break;
